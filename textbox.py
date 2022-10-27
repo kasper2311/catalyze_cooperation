fab2e7cf7a7370c0dd7807fd2facfb3cc8771439
kasper2311 <74134418+kasper2311@users.noreply.github.com>
2022-10-27 14:48:39 +0530

    Create textbox.py

---
+++ textbox.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  3 13:34:47 2022
+
+@author: ADMIN
+"""
+
+import pygame
+
+class textbox:
+    
+    def __init__(self,placeholder_text,m_type,coords):
+        #Coords are a set of vec2s
+        xd = pygame.Rect(left, top, width, height)
